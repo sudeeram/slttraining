9eb00a9d9bb165449de74e847f08ced180c973a9
Sudeera Mudugamuwa <Sudeera@myhost.local>
2019-11-20 12:27:56 +0530

    from sudeera branch

--- demo.py
+++ demo.py
@@ -10,6 +10,15 @@ iosxeao_netconf = {
 	'password' : 'D_Vay!_10&',
 }
 
+iosxeao_ssh = {
+	'device_type'	: 'cisco_ios',
+	'ip' 			: 'ios-xe-mgmt.cisco.com',
+	'port'			: '8181',
+	'username'		: 'root',
+	'password'		: 'D_Vay!_10&'
+}
+
+
 def main():
 	router1 = {
 		'device_type'	: 'cisco_ios',
@@ -29,6 +38,11 @@ def main():
 	print ("Executing %s" %command)
 	output = r1_con.send_command(command)
 
+
+	# EXECUTE SHOW VERSION COMMAND
+	command = 'show version'
+	print ("Executing %s" %command)
+	output = r1_con.send_command(command)
 
 
 
